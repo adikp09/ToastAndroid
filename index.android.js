98e8bb61878182cba272e6ef41ae7949e0d6f89b
Adi prasetyo <cahyo.adi@skyshi.com>
2017-08-01 11:45:44 +0700

    index android

--- index.android.js
+++ index.android.js
@@ -13,15 +13,45 @@ import {
   Modal,
   ToastAndroid
 } from 'react-native';
-import App from './app'
 
 export default class CustomNotification extends Component {
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      message: this.props.message
+    }
+  }
+
+  actions () {
+    return (
+      ToastAndroid.show(this.state.message, ToastAndroid.LONG)
+    )
+  }
+  
   render () {
     return (
-      <App message={'Thank You!'} textOnpress={'Click Me!'} />
+      <View style={styles.container}>
+        <Text onPress={() => this.actions()} style={styles.welcome}>
+          {this.props.textOnpress}
+        </Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
 
 AppRegistry.registerComponent('CustomNotification', () => CustomNotification);
